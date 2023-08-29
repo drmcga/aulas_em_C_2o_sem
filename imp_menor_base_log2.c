d683ab0c3e3c190366b6fc4de447cd7fba3fc9ce
Bianchin Gabriel <64701716+drmcga@users.noreply.github.com>
2023-08-29 16:40:02 -0300

    Create imp_menor_base_log2.c
    versao melhorada mais rapida e menos confusa do codigo imp_menor_base_log.c

---
+++ imp_menor_base_log2.c
@@ -0,0 +1,41 @@
+/*!
+ * ENUNCIADO
+ *
+ *  Escreva uma função que recebe um valor inteiro positivo n como parâmetro e devolve um
+ *  valor inteiro b tal que b^k = n para algum inteiro k, e b seja o menor possível. Por exemplo,
+ *  se n = 27 então o valor devolvido deve ser 3, já se n = 12 então o valor devolvido deve ser
+ *  12. Não use funções de bibliotecas na sua solução. O protótipo da função deve ser:
+ *
+ *  int menor_base_log(int n);
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int menor_base_log(int n);
+
+int main(){
+
+    int num, resu;
+    printf("Insira seu numero: ");
+    scanf("%d",&num);
+
+    resu = menor_base_log(num);
+
+    printf("\nResultado: %d",resu);
+
+}
+int menor_base_log(int n){
+
+    if (n==1) return 1;
+
+    int aux=1;
+    for (int i=2;i<n;i++){
+        while (aux<n){
+            aux*=i;
+        }if (aux==n){
+            return i;
+        }
+        aux=1;
+    }
+}
